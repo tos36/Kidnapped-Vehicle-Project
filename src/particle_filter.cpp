98fb0d55c1692745214f2984ec19daf13d14e19e
tos36 <toshiyuki.kaji36@gmail.com>
2019-07-29 06:34:55 +0000

    update ParticleFilter::init

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,7 +30,24 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 10;  // TODO: Set the number of particles
+  
+  // Gaussian distribution for x, y, theta
+  default_random_engine gen;
+  normal_distribution<double> dist_x(x, std[0]);
+  normal_distribution<double> dist_y(y, std[1]);
+  normal_distribution<double> dist_theta(theta, std[2]);
+  
+  vector<Particle> list_particles; //list of particles
+  for(int i=0; i < num_particles; ++i){
+  	Particle temp = {};
+  	temp.id = i;
+    temp.x = dist_x(gen);
+    temp.y = dist_y(gen);
+    temp.theta = dist_theta(gen);
+    temp.weight = 1;
+    list_particles.push_back(temp);
+  }
 
 }
 
